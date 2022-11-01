92a69e53c74f4090e062128e4b62d9c826156118
amba113 <64231693+amba113@users.noreply.github.com>
2022-11-01 07:36:29 -0400

    Fixed player ball wall collisions

--- PlayerBall.py
+++ PlayerBall.py
@@ -109,13 +109,10 @@ class PlayerBall(Ball):
             if self.rect.left < other.rect.right:
                 if self.rect.bottom > other.rect.top:
                     if self.rect.top < other.rect.bottom:
-                        if self.speedx > 0 and self.rect.right > other.rect.left:
-                            self.rect.right = other.rect.left - 1
-                        if self.speedx < 0:
-                            self.rect.left = other.rect.right + 1
-                        if self.speedy < 0:
-                            self.rect.top = other.rect.bottom - 1
-                        if self.speedy > 0:
-                            self.rect.bottom = other.rect.top + 1
+                        self.speedx = -self.speedx
+                        self.speedy = -self.speedy
+                        self.move()
+                        self.speedx = 0
+                        self.speedy = 0
                         return True
         return False
